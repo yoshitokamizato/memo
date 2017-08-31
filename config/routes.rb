3a61fdfac3bdab4fbe3d0ca6d8f77c67fc6fb353
Yoshito Kamizato <yuyueureka157@gmail.com>
2017-08-31 19:25:28 +0900

    edit root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: '/main#main'
+  root to: 'main#main'
   get '/main' => 'main#main'
   get '/main/new' => 'main#new'
   post '/main' => 'main#create'
